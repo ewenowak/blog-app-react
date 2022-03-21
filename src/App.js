4f08d6da38c8a3a6141320a3fa29330c87f9d5fe
Ewelina Nowak <ewel.nowak@gmail.com>
2022-03-21 23:06:19 +0100

    Add Container in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import PostAdd from './components/pages/PostAdd';
-import PostEdit from './components/pages/PostEdit';
-import PostPage from './components/pages/PostPage';
-import About from './components/pages/About';
-import Header from './components/views/Header';
+import NavBar from './components/views/NavBar';   
 import Footer from './components/views/Footer';
+import { Container } from 'react-bootstrap';
+import PostPage from './components/pages/PostPage';
+import PostEdit from './components/pages/PostEdit';
+import About from './components/pages/About'
+
 
 function App() {
   return (
     <main>
-      <Header />
-      <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/post/:id" element={ <PostPage /> } />
-        <Route path="/post/add" element={ <PostAdd /> } />
-        <Route path="/post/edit/:id" element={ <PostEdit /> } />
-        <Route path="/about" element={ <About /> } />
-      </Routes>
-      <Footer />
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/post/:id" element={ <PostPage /> } />
+          <Route path="/post/add" element={ <PostAdd /> } />
+          <Route path="/post/edit/:id" element={ <PostEdit /> } />
+          <Route path="/about" element={ <About /> } />
+        </Routes>
+        <Footer />
+      </Container>
     </main>
   );
 };
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,16 +1,16 @@
-import { Container, Button, Row, Col} from 'react-bootstrap';
+import {Button, Row, Col} from 'react-bootstrap';
 import AllPosts from '../features/AllPosts';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-            <Container>
-                <Row  xs={"auto"} className="justify-content-between mb-3">
-                   <Col as={"h2"}>All Posts</Col>
-                    <Col className="text-right"><Button as={Link} to={"/post/add"} variant="outline-info" >Add post</Button></Col>
-                </Row>
-                <AllPosts />
-            </Container>
+        <>
+            <Row  xs={"auto"} className="justify-content-between mb-3">
+                <Col as={"h2"}>All Posts</Col>
+                <Col className="text-right"><Button as={Link} to={"/post/add"} variant="outline-info" >Add post</Button></Col>
+            </Row>
+            <AllPosts />
+        </>
     );
 };
 
